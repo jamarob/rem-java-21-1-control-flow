99d9f027c6eb038d67d1949ec3e52667d760131d
Jan <janmartinrobert@gmail.com>
2021-01-20 18:19:57 +0100

    Add threatLevel parameter to implementation

--- src/main/java/RoomCapacity.java
+++ src/main/java/RoomCapacity.java
@@ -1,7 +1,18 @@
 public class RoomCapacity {
 
-    public static String getStatus(int numberOfPeopleInRoom) {
-        if(numberOfPeopleInRoom > 30){
+    public static String getStatus(int numberOfPeopleInRoom, String threatLevel) {
+        int limit;
+        switch(threatLevel){
+            case "Green":
+                limit = 60;
+                break;
+            case "Yellow":
+                limit = 30;
+                break;
+            default:
+                limit = 0;
+        }
+        if(numberOfPeopleInRoom > limit){
             return "Too many people";
         }
         return "Capacity limit not reached";
