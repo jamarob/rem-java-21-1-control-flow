3a2b040677494db6bcc40f319fb174f69d93d702
Jan <janmartinrobert@gmail.com>
2021-01-20 18:19:28 +0100

    Update tests

--- src/test/java/RoomCapacityTest.java
+++ src/test/java/RoomCapacityTest.java
@@ -5,26 +5,69 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RoomCapacityTest {
 
     @Test
-    public void returnsTooManyPeopleIfThereAreMoreThan30PeopleInTheRoom(){
+    public void returnsTooManyPeopleIfThreatLevelIsRed(){
         // given
-        int numberOfPeople = 42;
+        String threatLevel = "Red";
+        int  numberOfPeople = 3;
 
         // when
-        String actual = RoomCapacity.getStatus(numberOfPeople);
+        String actual = RoomCapacity.getStatus(numberOfPeople, threatLevel);
 
         // then
         assertEquals("Too many people", actual);
     }
 
     @Test
-    public void returnsCapacityLimitNotReachedIfThereAreLessThan30PeopleInTheRoom(){
+    public void returnsTooManyPeopleIfThereAreMoreThan30PeopleInTheRoomAndThreatLevelIsYellow(){
         // given
+        String threatLevel = "Yellow";
+        int numberOfPeople = 42;
+
+        // when
+        String actual = RoomCapacity.getStatus(numberOfPeople, threatLevel);
+
+        // then
+        assertEquals("Too many people", actual);
+    }
+
+    @Test
+    public void returnsCapacityLimitNotReachedIfThereAreLessThan30PeopleInTheRoomAndThreatLevelIsYellow(){
+        // given
+        String threatLevel = "Yellow";
         int numberOfPeople = 23;
 
         // when
-        String actual = RoomCapacity.getStatus(numberOfPeople);
+        String actual = RoomCapacity.getStatus(numberOfPeople, threatLevel);
 
         // then
         assertEquals("Capacity limit not reached", actual);
     }
+
+    @Test
+    public void returnsTooManyPeopleIfThereAreMoreThan60PeopleInTheRoomAndThreatLevelIsGreen(){
+        // given
+        String threatLevel = "Green";
+        int numberOfPeople = 66;
+
+        // when
+        String actual = RoomCapacity.getStatus(numberOfPeople, threatLevel);
+
+        // then
+        assertEquals("Too many people", actual);
+    }
+
+    @Test
+    public void returnsCapacityLimitNotReachedIfThereAreLessThan30PeopleInTheRoomAndThreatLevelIsGreen(){
+        // given
+        String threatLevel = "Green";
+        int numberOfPeople = 42;
+
+        // when
+        String actual = RoomCapacity.getStatus(numberOfPeople, threatLevel);
+
+        // then
+        assertEquals("Capacity limit not reached", actual);
+    }
+
+
 }
