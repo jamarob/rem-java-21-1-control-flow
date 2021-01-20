fd94a52262f6e4d47c5ca3f3c0292d3fee28d0e9
Jan <janmartinrobert@gmail.com>
2021-01-20 17:38:19 +0100

    Initial commit

---
+++ src/main/java/AppMain.java
@@ -0,0 +1,10 @@
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.println(getHelloWorld());
+    }
+
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+}
